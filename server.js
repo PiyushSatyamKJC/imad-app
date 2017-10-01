74914981c2adf611b4da169f53252e115bcaf77f
PiyushSatyamKJC <30688396+PiyushSatyamKJC@users.noreply.github.com>
2017-10-01 08:33:42 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,79 +4,11 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articles = {
-    'article-one': {
-    title:'Article One | PIYUSH SATYAM',
-    heading:'Article One',
-    date:'Aug 24,2017',
-    content:`<p>
-                content of the article
-            </p>`
-    },
-    'article-two': {
-        title:'Article Two | PIYUSH SATYAM',
-    heading:'Article Two',
-    date:'Aug 24,2017',
-    content:`<p>
-                content of the article
-            </p>`
-    },
-    'article-three': {
-        title:'Article Three | PIYUSH SATYAM',
-    heading:'Article Three',
-    date:'Aug 24,2017',
-    content:`<p>
-                content of the article
-            </p>`
-    }
-};
-function createTemplate(data) {
-var title = data.title;
-var date = data.date;
-var heading = data.heading;
-var content = data.content;
 
-var htmltemplate =
-`<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width-device-width,initial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-        
-    </head>
-    <body>
-        <div class="container">
-            <div>
-            <a href="/">HOME</a>
-            </div>
-        <hr />
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-            <p>
-                ${content}
-            </p>
-        </div>   
-        </div>
-    </body>
-</html>`;
-return htmltemplate;
-}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName',function(req,res) {
-    //articleName == article-one
-    //articles[articleName] == {} content object for article one
-    var articleName =req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-})
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
