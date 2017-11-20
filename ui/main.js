ca4eae42b5aec6c55fb7e752ca9622b50e3caf3e
PiyushSatyamKJC <30688396+PiyushSatyamKJC@users.noreply.github.com>
2017-11-20 02:14:49 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,13 +3,12 @@ var button = document.getElementById('counter');
 button.onclick = function() {
   //Create a Request Object
   var request = new XMLHttpRequest();
-  
   //Capture the Response and store it in a Variable
   request.onreadystatechange = function() {
     if(request.readyState == XMLHttpRequest.DONE) {
         //Take Some Action
         if(request.status == 200) {
-            var counter = request.responseText;
+           var counter = request.responseText;
             var span = document.getElementById('count');
              span.innerHTML = counter.toString();
         }
@@ -19,20 +18,4 @@ button.onclick = function() {
   //Make the Request
   request.open('GET','http://satyampiyush790.imad.hasura-app.io/counter',true);
   request.send(null);
-  
-};
-//Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit.btn');
-submit.onclick = function() {
-  //Make a Request to the server and send the name
-  //Capture a list of names and render it as a list
-  var names = ['name1','name2','name3','name4'];
-  var list = '';
-  for(var i=0;i<names.length;i++) {
-      list = '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
 };
