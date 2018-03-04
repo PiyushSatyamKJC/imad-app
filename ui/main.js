3ed657e9cae24aa97dcbc80bec81170ba98b9e7d
PiyushSatyamKJC <30688396+PiyushSatyamKJC@users.noreply.github.com>
2018-03-04 00:26:04 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //Change the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML='New value';
+//Move the image
+var img=document.getElementById('madi');
+img.onclick=function() {
+  img.style.marginLeft='100px';  
+};
