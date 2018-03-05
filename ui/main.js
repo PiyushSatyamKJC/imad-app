0b7eb06984e12cf8ef73a47d7b5e6217a113fcee
PiyushSatyamKJC <30688396+PiyushSatyamKJC@users.noreply.github.com>
2018-03-05 08:12:41 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,14 @@
-//Counter code
-var button=document.getElementById('counter');
-button.onclick=function() {
-  //Create a request object
-  var request=new XMLHttpRequest();
-  //Capture the response and store it in a variable
-  request.onreadystatechange=function() {
-    if(request.readystate==XMLHttpRequest.DONE) {
-        //Take some action
-        if(request.status==200) {
-            var counter=request.responseText;
-            var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
-        }
-    }  
-    //Not done yet
-  };
-  //Make the request
-  request.open('GET','http://satyampiyush16jan1987.imad.hasura-app.io/counter',true);
-  request.send(null);
+//Change the text of main-text div
+var element = document.getElementById('main-text');
+element.innerHTML='New value';
+//Move the image
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight() {
+  marginLeft=marginLeft+1;
+  img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function() {
+  var interval=setInterval(moveRight,50);
+  img.style.marginLeft='100px';  
 };
